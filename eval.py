cfbed5a0c9fd1d49812450bbfdbbaa4d2886e442
Vladislav Pikulin <vp111@students.uni-freiburg.de>
2025-07-03 11:57:32 +0200

    added params for seeding and splitting train data

--- eval.py
+++ eval.py
@@ -7,11 +7,12 @@ from importlib import import_module
 import torch
 
 @hydra.main(version_base=None, config_path="./conf/", config_name="config")
+@hydra.main(version_base=None, config_path="./conf/", config_name="config")
 def main(conf):
     pl.seed_everything(conf.seed)
 
-    log_base_dir = "/".join( conf.checkpoint.split("/")[:-2] ) + "/"
     checkpoint = to_absolute_path(conf.checkpoint)
+    print(checkpoint)
     assert os.path.exists(checkpoint), f"Checkpoint {checkpoint} does not exist"
 
     model_path = conf.model.target._target_
@@ -36,8 +37,7 @@ def main(conf):
 
     trainer = pl.Trainer(
         logger=False,
-        accelerator=accelerator,
-        devices=1,
+
         max_epochs=1,
         limit_val_batches=conf.limit_val_batches,
         limit_test_batches=conf.limit_test_batches,
@@ -50,6 +50,7 @@ def main(conf):
         trainer.validate(model, datamodule)
     else:
         trainer.test(model, datamodule)
+
 
 
 if __name__ == "__main__":
--- src/datamodule/av2_datamodule.py
+++ src/datamodule/av2_datamodule.py
@@ -4,6 +4,9 @@ from typing import Optional
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader as TorchDataLoader
 
+import torch
+from torch.utils.data import random_split
+
 from .av2_dataset import Av2Dataset, collate_fn
 
 
@@ -19,6 +22,8 @@ class Av2DataModule(LightningDataModule):
         num_workers: int = 8,
         pin_memory: bool = True,
         test: bool = False,
+        train_fraction: float = 0.5, # fraction of train data to use
+        split_seed: int = 42,         # seed for splitting
     ):
         super(Av2DataModule, self).__init__()
         self.data_root = Path(data_root)
@@ -30,12 +35,25 @@ class Av2DataModule(LightningDataModule):
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.test = test
+        # new params
+        self.train_fraction = train_fraction
+        self.split_seed = split_seed
 
     def setup(self, stage: Optional[str] = None) -> None:
         if not self.test:
-            self.train_dataset = Av2Dataset(
+            full_train_dataset = Av2Dataset(
                 data_root=self.data_root / self.data_folder, cached_split="train"
             )
+            if self.train_fraction < 1.0:
+                train_len = int(len(full_train_dataset) * self.train_fraction)
+                rest_len = len(full_train_dataset) - train_len
+                generator = torch.Generator().manual_seed(self.split_seed)
+                self.train_dataset, _ = random_split(
+                    full_train_dataset, [train_len, rest_len], generator=generator
+                )
+            else:
+                self.train_dataset = full_train_dataset
+
             self.val_dataset = Av2Dataset(
                 data_root=self.data_root / self.data_folder, cached_split="val"
             )
